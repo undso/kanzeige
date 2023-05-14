caaf7a596e48f0e2b74c9b157799b5ed01624bf9
Alexander Friedrichs <undso@users.noreply.github.com>
2023-05-14 21:22:41 +0200

    Import from Spring Initializr

---
+++ src/main/java/de/friedrichs/kanzeige/KanzeigeApplication.java
@@ -0,0 +1,13 @@
+package de.friedrichs.kanzeige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanzeigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanzeigeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/friedrichs/kanzeige/KanzeigeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.friedrichs.kanzeige;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanzeigeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
